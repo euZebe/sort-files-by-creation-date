067c5f4afa77c4ee745808579ad84ebbb2fe6f04
euzebe <euzebe@axiome.me>
2018-04-04 13:52:00 +0200

    implements files move according to date criteria

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const { isDirectory, getFiles } = require('./utils');
+const { DateTime } = require('luxon');
+const { basename } = require('path');
+
+const {
+  isDirectory,
+  getFiles,
+  getFileCreationDate,
+  isWeekend,
+  isWorkDayEvening,
+  moveFileTo,
+  createDirIfNeeded,
+  WEEKEND_FOLDER,
+  EVENING_FOLDER,
+  WORK_FOLDER,
+} = require('./utils');
+
+const startDate = DateTime.local();
 
 // read argument
 const directory = process.argv[2];
@@ -10,6 +26,33 @@ isDirectory(directory);
 
 // list files to sort
 const files = getFiles(directory);
-console.log(`going to sort ${files.length} file(s) into ${directory}`);
+console.log(`${files.length} fichier(s) à trier dans ${directory}`);
 
-// TODO: for each, get their creation date
+files.map(f => {
+  // for each, get their creation date
+  const creationDate = getFileCreationDate(f);
+
+  // TODO: deal with timezones
+
+  // determine if date is a weekend
+  if (isWeekend(creationDate)) {
+    // TODO move to ${directory}/_weekend
+    createDirIfNeeded(directory, WEEKEND_FOLDER);
+    moveFileTo(f, directory, WEEKEND_FOLDER);
+    console.log(basename(f), 'créé un', creationDate.get('weekdayLong'), `déplacé dans ${WEEKEND_FOLDER}`);
+
+  } else if (isWorkDayEvening(creationDate)) {
+    // TODO move to ${directory}/_evening_of_working_days
+    createDirIfNeeded(directory, EVENING_FOLDER);
+    moveFileTo(f, directory, EVENING_FOLDER);
+    console.log(basename(f), 'créé à', `${creationDate.get('hour')}:${creationDate.get('minute')}`, `déplacé dans ${EVENING_FOLDER}`);
+  } else {
+    // TODO move to ${directory}/_working_hours
+    createDirIfNeeded(directory, WORK_FOLDER);
+    moveFileTo(f, directory, WORK_FOLDER);
+    console.log(basename(f), 'créé un', `${creationDate.get('weekdayLong')} à ${creationDate.get('hour')}:${creationDate.get('minute')}`, `déplacé dans ${WORK_FOLDER}`);
+  }
+});
+
+const endDate = DateTime.local();
+console.log('tâche terminée en ', endDate.diff(startDate).milliseconds / 1000, 'secondes.');
--- utils.js
+++ utils.js
@@ -1,9 +1,40 @@
-const { lstatSync, readdirSync } = require('fs');
-const { join } = require('path');
+const { lstatSync, readdirSync, renameSync, mkdirSync } = require('fs');
+const { join, basename } = require('path');
+const { DateTime } = require('luxon');
 
 
 module.exports.isDirectory = source => lstatSync(source).isDirectory();
 const isFile = source => lstatSync(source).isFile();
 
-module.exports.getFiles = source => readdirSync(source).map(name => join(source, name)).filter(isFile);
+module.exports.getFiles = source => readdirSync(source)
+  .map(f => join(source, f))
+  .filter(isFile);
 
+module.exports.getFileCreationDate = source => DateTime.fromMillis(lstatSync(source).mtime);
+
+
+module.exports.isWeekend = date => date.get('weekday') >= 6;
+module.exports.isWorkDayEvening = date => date.get('weekday') < 6 && (
+  date.get('hour') > 18 // UTC => 20h
+  || date.get('hour') < 5  // UTC => 7h
+);
+
+module.exports.moveFileTo = (filePath, directory, subDirectory) => {
+  const fileName = basename(filePath);
+  renameSync(
+    join(directory, fileName),
+    join(directory, subDirectory, fileName)
+  );
+};
+
+module.exports.createDirIfNeeded = (source, subDirectory) => {
+  try {
+    lstatSync(join(source, subDirectory));
+  } catch (e) {
+    mkdirSync(join(source, subDirectory));
+  }
+};
+
+module.exports.WEEKEND_FOLDER = '_weekend';
+module.exports.EVENING_FOLDER = '_evening_of_working_days';
+module.exports.WORK_FOLDER = '_working_hours';
